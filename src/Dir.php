ba96771950b8613688643967a59600ee13985454
Grigorii Duca <contact@greg.md>
2016-11-06 13:50:35 +0200

    .

--- src/Dir.php
+++ src/Dir.php
@@ -32,4 +32,40 @@ class Dir
 
         return true;
     }
+
+    public static function copy($source, $dest, $permissions = 0755)
+    {
+        // Check for symlinks
+        if (is_link($source)) {
+            return symlink(readlink($source), $dest);
+        }
+
+        // Simple copy for a file
+        if (is_file($source)) {
+            return copy($source, $dest);
+        }
+
+        // Make destination directory
+        if (!is_dir($dest)) {
+            mkdir($dest, $permissions);
+        }
+
+        // Loop through the folder
+        $dir = dir($source);
+
+        while (false !== $entry = $dir->read()) {
+            // Skip pointers
+            if ($entry == '.' || $entry == '..') {
+                continue;
+            }
+
+            // Deep copy directories
+            static::copy("$source/$entry", "$dest/$entry", $permissions);
+        }
+
+        // Clean up
+        $dir->close();
+
+        return true;
+    }
 }
