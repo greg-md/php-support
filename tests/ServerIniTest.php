48cc2d230782dae57c5dfad9a8577d48df9a2f4e
Grigorii Duca <contact@greg.md>
2017-01-03 23:57:08 +0200

    .

--- tests/ServerIniTest.php
+++ tests/ServerIniTest.php
@@ -12,4 +12,40 @@ class ServerIniTest extends TestCase
     {
         $this->assertArrayHasKey('error_reporting', ServerIni::getAll());
     }
+
+    /** @test */
+    public function it_gets_error_reporting()
+    {
+        $this->assertEquals(error_reporting(), ServerIni::get('error_reporting'));
+    }
+
+    /** @test */
+    public function it_throws_an_exception_when_get_wrong_value()
+    {
+        $this->expectException(\Exception::class);
+
+        ServerIni::get('some_wrong_value');
+    }
+
+    /** @test */
+    public function it_throws_an_exception_when_set_wrong_value()
+    {
+        $this->expectException(\Exception::class);
+
+        ServerIni::set('some_wrong_value', true);
+    }
+
+    /** @test */
+    public function it_sets_values()
+    {
+        ServerIni::set('error_reporting', 1);
+
+        $this->assertEquals(1, error_reporting());
+
+        ServerIni::setMore([
+            'memory_limit' => '1024M',
+        ]);
+
+        $this->assertEquals('1024M', ini_get('memory_limit'));
+    }
 }
